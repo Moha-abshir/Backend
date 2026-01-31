928d0ab48f2090ecfec5fff3a31069ba96532030
Mohammed Abshir <moabshir06@gmail.com>
2026-01-31 17:57:36 +0300

    testing the writeFileSync and it works

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const {readFileSync, writeFileSync} = require('fs');
-const { encode } = require('punycode');
 
 const first = readFileSync('./text1.txt', 'utf8')
 console.log(first);
@@ -8,9 +7,10 @@ const newAddedContent = 'This is the new content after the file has been changed
 writeFileSync('./text2.txt', 
    newAddedContent,
    {
-      // encoding: 'utf8', //Node translates the text to utf8
-      // flag: 'a',//This one here adds the new content at the end of the old text.
-      // //flag: 'w'
-      // mode: '0o666'
+      encoding: 'utf8', //Node translates the text to utf8
+      flag: 'a',//This one here adds the new content at the end of the old text.
+      //flag: 'w', This one here overrites the entire file.
+      //flag: 'r', It opens the file in reading mode. You will get an error if you try to edit/write in this file in the future
    } 
-)
+);
+console.log(readFileSync('./text2.txt','utf8'))

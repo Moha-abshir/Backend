65fe32143481738f05debd71d634b0d963e71988
Mohammed Abshir <moabshir06@gmail.com>
2026-01-31 13:00:23 +0300

    add alternative-syntax module and update app.js to include data logging

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const names = require('./sharing&secret');//require is how Node.js loads code from somewhere else(sharing&secret) and uses it in the current running app.
 const sayHi = require('./utils');
 console.log(names);
+const data = require("./alternative-syntax")
+console.log(data);
+console.log(data)
 
 sayHi(names.john);
 sayHi(names.peter);
